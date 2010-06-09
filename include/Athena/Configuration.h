3a30ef492ad71ea012875152910a9962ec325f65
Philip Abbet <philip.abbet@gmail.com>
2010-06-09 08:47:40 +0200

    Add the class 'Configuration'

---
+++ include/Athena/Configuration.h
@@ -0,0 +1,145 @@
+/** @file	Configuration.h
+	@author	Philip Abbet
+
+	Declaration of the class 'Athena::Configuration'
+*/
+
+#ifndef _ATHENA_CONFIGURATION_H_
+#define _ATHENA_CONFIGURATION_H_
+
+#include <Athena/Prerequisites.h>
+
+namespace Athena {
+
+//-----------------------------------------------------------------------------------
+/// @brief	Contains the configuration of Athena
+//-----------------------------------------------------------------------------------
+struct tAthenaConf
+{
+	std::string	    strOgreConfigFile;	///< Name of the Ogre configuration file
+	std::string	    strPluginsFile;		///< Name of the file containing the list of plugins to load
+	std::string	    strResourcesFile;	///< Name of the file containing the list of paths to the resources
+};
+
+
+//-----------------------------------------------------------------------------------
+/// @brief	Contains the configuration of the logging
+//-----------------------------------------------------------------------------------
+struct tLogConf
+{
+	std::string	    strAthenaLogFile;	///< Name of the log file of Athena
+	std::string	    strAthenaLogKind;	///< Kind of the log file of Athena
+	std::string	    strOgreLogFile;		///< Name of the log file of Ogre
+	bool			bRedirectOgreLog;	///< Indicates if the Ogre log must be redirected into the Athena's one
+};
+
+
+//-----------------------------------------------------------------------------------
+/// @brief	Contains the configuration of the GUI subsystem
+//-----------------------------------------------------------------------------------
+// struct tGUIConf
+// {
+//  bool            bEnable;                    ///< Indicates if the GUI subsystem is enabled
+//  std::string     strRuntimeDir;              ///< Runtime directory
+//  std::string     strProfileDir;              ///< Profile directory
+//  std::string     strLocalDir;                ///< Local directory
+//  std::string     strDefaultCursor;           ///< Default cursor
+// };
+
+//-----------------------------------------------------------------------------------
+/// @brief	Contains the configuration of the Inputs subsystem
+//-----------------------------------------------------------------------------------
+struct tInputsConf
+{
+	bool			bEnable;					///< Indicates if the Inputs subsystem is enabled
+//	std::string	    strVirtualControllersFile;	///< Name of the file containing the virtual controllers
+};
+
+//-----------------------------------------------------------------------------------
+/// @brief	Contains the configuration of the default window
+//-----------------------------------------------------------------------------------
+struct tDefaultWindowConf
+{
+	bool			bEnable;					///< Indicates if a default window must be created
+	std::string	    strTitle;					///< Title of the window
+};
+
+//-----------------------------------------------------------------------------------
+/// @brief	Contains the configuration of the Audio subsystem
+//-----------------------------------------------------------------------------------
+// struct tAudioConf
+// {
+//  bool            bEnable;                    ///< Indicates if the Audio subsystem is enabled
+// };
+
+//-----------------------------------------------------------------------------------
+/// @brief	Contains the configuration of the Physics subsystem
+//-----------------------------------------------------------------------------------
+// struct tPhysicsConf
+// {
+//  bool            bEnable;                    ///< Indicates if the Physics subsystem is enabled
+//  bool            bCollisionManagerEnable;    ///< Indicates if the collisions manager is enabled
+// };
+
+//-----------------------------------------------------------------------------------
+/// @brief	Contains the configuration of the Scripting subsystem
+//-----------------------------------------------------------------------------------
+// struct tScriptingConf
+// {
+//  bool            bEnable;                    ///< Indicates if the Scripting subsystem is enabled
+//  std::string     strStartupScript;           ///< Name of the script to execute at startup
+//  bool            bEmbedded;                  ///< Indicates if the Scripting subsystem is embedded
+// };
+
+//-----------------------------------------------------------------------------------
+/// @brief	Contains the configuration of the Network subsystem
+//-----------------------------------------------------------------------------------
+// struct tNetworkConf
+// {
+//  bool            bEnable;                    ///< Indicates if the Network subsystem is enabled
+// };
+
+
+//---------------------------------------------------------------------------------------
+/// @brief	Contains the configuration of the engine
+//---------------------------------------------------------------------------------------
+class ATHENA_SYMBOL Configuration
+{
+	//_____ Construction / Destruction __________
+public:
+    //-----------------------------------------------------------------------------------
+    /// @brief	Constructor
+    //-----------------------------------------------------------------------------------
+	Configuration();
+
+    //-----------------------------------------------------------------------------------
+    /// @brief	Destructor
+    //-----------------------------------------------------------------------------------
+	~Configuration();
+
+
+	//_____ Methods __________
+public:
+    //-----------------------------------------------------------------------------------
+    /// @brief	Load a configuration file
+    /// @param	strConfigFile	The name of the configuration file to use
+    //-----------------------------------------------------------------------------------
+	void load(const std::string& strConfigFile);
+
+
+	//_____ Attributes __________
+public:
+	tAthenaConf			athena;			///< Athena configuration
+	tLogConf			log;			///< Log configuration
+    // tGUIConf         gui;            ///< GUI configuration
+	tInputsConf			inputs;			///< Inputs configuration
+	tDefaultWindowConf	defaultWindow;	///< Default window configuration
+    // tAudioConf          audio;          ///< Audio configuration
+    // tPhysicsConf        physics;        ///< Physics configuration
+    // tScriptingConf      scripting;      ///< Scripting configuration
+    // tNetworkConf        network;        ///< Network configuration
+};
+
+}
+
+#endif
---
+++ src/Configuration.cpp
@@ -0,0 +1,186 @@
+/**	@file	Configuration.cpp
+	@author	Philip Abbet
+
+	Implementation of the class 'Athena::Configuration'
+*/
+
+#include <Athena/Configuration.h>
+#include <Athena-Core/Data/ConfigFile.h>
+#include <Ogre/OgreException.h>
+
+using namespace Athena;
+using namespace Athena::Data;
+using namespace std;
+
+
+/****************************** CONSTRUCTION / DESTRUCTION *****************************/
+
+Configuration::Configuration()
+{
+	// Athena configuration defaults
+	athena.strOgreConfigFile			= "";
+	athena.strPluginsFile				= "plugins.cfg";
+	athena.strResourcesFile				= "";
+
+	// Log configuration defaults
+	log.strAthenaLogFile				= "";
+	log.strAthenaLogKind				= "XML";
+	log.strOgreLogFile					= "";
+	log.bRedirectOgreLog				= true;
+
+	// GUI configuration defaults
+    // gui.bEnable                         = true;
+    // gui.strRuntimeDir                   = "Runtime";
+    // gui.strProfileDir                   = "Profile";
+    // gui.strLocalDir                     = "";
+    // gui.strDefaultCursor                = "";
+
+	// Inputs configuration defaults
+	inputs.bEnable						= true;
+    // inputs.strVirtualControllersFile = "";
+
+	// Default window configuration defaults
+	defaultWindow.bEnable				= true;
+	defaultWindow.strTitle				= "";
+
+	// Audio configuration defaults
+    // audio.bEnable                        = true;
+
+	// Physics configuration defaults
+    // physics.bEnable                      = true;
+    // physics.bCollisionManagerEnable      = true;
+
+	// Scripting configuration defaults
+    // scripting.bEnable                    = true;
+    // scripting.bEmbedded                  = true;
+    // scripting.strStartupScript           = "";
+
+	// Network configuration defaults
+    // network.bEnable                      = true;
+}
+
+//---------------------------------------------------------------------
+
+Configuration::~Configuration()
+{
+}
+
+
+/*************************************** METHODS ***************************************/
+
+void Configuration::load(const std::string& strConfigFile)
+{
+	// Assertions
+	assert(!strConfigFile.empty() && "No configuration file supplied");
+
+	// Declarations
+	ConfigFile cfgFile;
+
+	// Open the configuration file
+	if (!cfgFile.load(strConfigFile))
+		OGRE_EXCEPT(0, "Failed to load the configuration file '" + strConfigFile + "'", __FUNCTION__);
+
+	// Athena section
+	if (cfgFile.selectSection("Athena"))
+	{
+		// Required parameters
+		if (!cfgFile.getParameterValue("Config", athena.strOgreConfigFile))
+			OGRE_EXCEPT(0, "Missing parameter: Ogre's configuration file", __FUNCTION__);
+
+		// Optional parameters
+		cfgFile.getParameterValue("Plugins",	athena.strPluginsFile);
+		cfgFile.getParameterValue("Resources",	athena.strResourcesFile);
+	}
+	else
+	{
+		OGRE_EXCEPT(0, "Missing section: Athena", __FUNCTION__);
+	}
+
+
+	// Log section
+	if (cfgFile.selectSection("Log"))
+	{
+		// Optional parameters
+		cfgFile.getParameterValue("Athena",				log.strAthenaLogFile);
+		cfgFile.getParameterValue("Kind",				log.strAthenaLogKind);
+		cfgFile.getParameterValue("Ogre",				log.strOgreLogFile);
+		cfgFile.getParameterValue("RedirectOgreLog",	log.bRedirectOgreLog);
+	}
+
+
+	// GUI section
+    // if (cfgFile.selectSection("GUI"))
+    // {
+    //  // Optional parameters
+    //  cfgFile.getParameterValue("Enable", gui.bEnable);
+    //  cfgFile.getParameterValue("RuntimeDir", gui.strRuntimeDir);
+    //  cfgFile.getParameterValue("ProfileDir", gui.strProfileDir);
+    // 
+    //  // Required parameters if enabled
+    //  if (gui.bEnable)
+    //  {
+    //      if (!cfgFile.getParameterValue("LocalDir", gui.strLocalDir))
+    //      {
+    //          OGRE_EXCEPT(0, "Missing parameter: GUI's local directory", __FUNCTION__);
+    //      }
+    //      
+    //      if (!cfgFile.getParameterValue("DefaultCursor", gui.strDefaultCursor))
+    //      {
+    //          OGRE_EXCEPT(0, "Missing parameter: GUI's default cursor", __FUNCTION__);
+    //      }
+    //  }
+    // }
+
+
+	// Inputs section
+	if (cfgFile.selectSection("Inputs"))
+	{
+		// Optional parameters
+		cfgFile.getParameterValue("Enable",				inputs.bEnable);
+        // cfgFile.getParameterValue("VirtualControllers",  inputs.strVirtualControllersFile);
+	}
+
+
+	// DefaultWindow section
+	if (cfgFile.selectSection("DefaultWindow"))
+	{
+		// Optional parameters
+		cfgFile.getParameterValue("Enable",	defaultWindow.bEnable);
+		cfgFile.getParameterValue("Title",	defaultWindow.strTitle);
+	}
+
+
+	// Audio section
+    // if (cfgFile.selectSection("Audio"))
+    // {
+    //  // Optional parameters
+    //  cfgFile.getParameterValue("Enable", audio.bEnable);
+    // }
+
+
+	// Physics section
+    // if (cfgFile.selectSection("Physics"))
+    // {
+    //  // Optional parameters
+    //  cfgFile.getParameterValue("Enable",             physics.bEnable);
+    //  cfgFile.getParameterValue("CollisionManager",   physics.bCollisionManagerEnable);
+    // }
+
+
+	// Scripting section
+    // if (cfgFile.selectSection("Scripting"))
+    // {
+    //  // Optional parameters
+    //  cfgFile.getParameterValue("Enable",         scripting.bEnable);
+    //  cfgFile.getParameterValue("StartupScript",  scripting.strStartupScript);
+    //  cfgFile.getParameterValue("Embedded",       scripting.bEmbedded);
+    // }
+
+
+	// Network section
+    // if (cfgFile.selectSection("Network"))
+    // {
+    //  // Optional parameters
+    //  cfgFile.getParameterValue("Enable", network.bEnable);
+    // }
+}
